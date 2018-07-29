0b5b7a2d381cd9ca59ac131a89de0e2a880d644e
blowedminds <ruzgardeniz.08@hotmail.com>
2018-07-29 16:35:37 +0300

    Role permission relation improved

--- Http/Controllers/RoleController.php
+++ Http/Controllers/RoleController.php
@@ -4,6 +4,7 @@ namespace App\Modules\Editor\Admin\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Modules\Core\Role;
+use App\Modules\Core\RolePermission;
 
 class RoleController extends Controller
 {
@@ -14,7 +15,7 @@ class RoleController extends Controller
 
     public function getRoles()
     {
-        return response()->json(Role::all());
+        return response()->json(Role::with('permissions')->get());
     }
 
     public function getRole($id)
@@ -25,10 +26,31 @@ class RoleController extends Controller
     public function postRole($id)
     {
         request()->validate([
-
+            'name' => 'required',
+            'slug' => 'required',
+            'description' => 'required',
+            'permissions' => 'required|array'
         ]);
 
         $role = Role::findOrFail($id);
+
+        RolePermission::where('role_id', $role->id)->forceDelete();
+
+        foreach (request()->get('permissions') as $key => $value) {
+            RolePermission::create([
+                'role_id' => $role->id,
+                'permission_id' => $value
+            ]);
+        }
+
+        $role->fill(request()->only(['name', 'slug', 'description']));
+
+        return response()->json([
+            'header' => 'İşlem Başarılı',
+            'message' => 'Rol güncellendi',
+            'state' => 'success',
+            'action' => 'Tamam'
+        ]);
     }
 
     public function putRole()
