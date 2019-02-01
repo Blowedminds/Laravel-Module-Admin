0d9255408d9f59aeeb692f09b9ec1f3e401e4289
blowedminds <ruzgardeniz.08@hotmail.com>
2019-02-01 15:49:25 +0300

    Role management completed

--- Http/Controllers/RoleController.php
+++ Http/Controllers/RoleController.php
@@ -55,11 +55,42 @@ class RoleController extends Controller
 
     public function putRole()
     {
+        request()->validate([
+            'name' => 'required',
+            'slug' => 'required',
+            'description' => 'required',
+            'permissions' => 'array|required'
+        ]);
 
+        $role = Role::create(request()->only(['name', 'slug', 'description']));
+
+        foreach (request()->input('permissions') as $permission) {
+            RolePermission::create([
+                'role_id' => $role->id,
+                'permission_id' => $permission
+            ]);
+        }
+        return response()->json([
+            'header' => 'İşlem Başarılı',
+            'message' => 'Rol Eklendi',
+            'state' => 'success',
+            'action' => 'Tamam'
+        ]);
     }
 
-    public function deleteRole()
+    public function deleteRole($id)
     {
+        $role = Role::findOrFail($id);
 
+        RolePermission::where('role_id', $role->id)->delete();
+
+        $role->delete();
+
+        return response()->json([
+            'header' => 'İşlem Başarılı',
+            'message' => 'Rol Silindi',
+            'state' => 'success',
+            'action' => 'Tamam'
+        ]);
     }
 }
